8fb7c187083206d8a28023fcc0cb1988d94a010d
Subham Dutta <dsubh269@gmail.com>
2026-02-14 00:13:17 +0530

    Time: 3 ms (26.86%), Space: 64.5 MB (92.06%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] answer = new int[nums.length];
-        int[] prefix = new int[nums.length];
+        // int[] prefix = new int[nums.length];
         int[] suffix = new int[nums.length];
 
         // prefix 
-        Arrays.fill(prefix, 1);
+        Arrays.fill(answer, 1);
         for (int i = 1; i < nums.length; i++) {
-            prefix[i] = prefix[i - 1] * nums[i - 1];
+            answer[i] = answer[i - 1] * nums[i - 1];
         }
 
         // suffix 
@@ -19,9 +19,8 @@ class Solution {
         }
 
         // multiply
-
         for (int i = 0; i < nums.length; i++){
-            answer[i] = prefix[i] * suffix[i];
+            answer[i] = answer[i] * suffix[i];
         }
 
             return answer;
