ec6c9f9121eda60c2b7d16576ae63d273432ec71
Subham Dutta <dsubh269@gmail.com>
2026-02-23 23:53:34 +0530

    Time: 0 ms (100%), Space: 43.5 MB (96.6%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        // Step 1: Count frequencies (assuming range 0-1000)
         int[] count = new int[1001];
         for (int num : arr1) {
             count[num]++;
@@ -9,15 +8,13 @@ class Solution {
         int[] result = new int[arr1.length];
         int index = 0;
 
-        // Step 2: Fill result based on arr2 order
         for (int num : arr2) {
             while (count[num] > 0) {
                 result[index++] = num;
                 count[num]--;
             }
         }
-
-        // Step 3: Fill remaining elements in ascending order
+        
         for (int i = 0; i < count.length; i++) {
             while (count[i] > 0) {
                 result[index++] = i;
